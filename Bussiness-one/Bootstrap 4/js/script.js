c2ee99447e4117f308650fd792d412146543e830
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-12-27 18:54:04 +0500

    Update Added Cutome Script File

--- Bussiness-one/Bootstrap 4/js/script.js
+++ Bussiness-one/Bootstrap 4/js/script.js
@@ -86,3 +86,17 @@ $(document).ready(function () {
 $(function () {
     $('a').smoothScroll();
 });
+/*======================= Owl Carousel =======================*/
+$(document).ready(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 1],
+        pagination: false,
+        navigation: true,
+        navigationText: ["", ""],
+        slideSpeed: 1000,
+        autoPlay: true
+    });
+});
